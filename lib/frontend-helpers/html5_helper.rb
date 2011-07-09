d4472f69cbe41ed000015aff7d1faf635aa0cae2
Christopher Hein <me@christopherhein.com>
2011-07-09 03:41:26 -0700

    html5 html_tag needed to be html_safe

--- lib/frontend-helpers/html5_helper.rb
+++ lib/frontend-helpers/html5_helper.rb
@@ -3,12 +3,12 @@ module FrontendHelpers
 
     def html_tag(attrs = {}, &block)
       attrs.symbolize_keys!
-      haml_concat("<!--[if lt IE 7]><html class='ie6 no-js'><![endif]-->")
-      haml_concat("<!--[if IE 7]><html class='ie7 no-js'><![endif]-->")
-      haml_concat("<!--[if IE 8]><html class='ie8 no-js'><![endif]-->")
-      haml_concat("<!--[if gt IE 8]><!-->")
+      haml_concat "<!--[if lt IE 7]><html class='ie6 no-js'><![endif]-->".html_safe
+      haml_concat "<!--[if IE 7]><html class='ie7 no-js'><![endif]-->".html_safe
+      haml_concat "<!--[if IE 8]><html class='ie8 no-js'><![endif]-->".html_safe
+      haml_concat "<!--[if gt IE 8]><!-->".html_safe
       haml_tag :html, attrs do
-        haml_concat("<!--<![endif]-->")
+        haml_concat "<!--<![endif]-->".html_safe
         block.call
       end
     end
