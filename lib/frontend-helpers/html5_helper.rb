23b6adb31186f2d967843403be459e4036936568
Christopher Hein <me@christopherhein.com>
2011-07-09 03:39:42 -0700

    Edited the html5 helpers and added some information into the readme to and added license file

--- lib/frontend-helpers/html5_helper.rb
+++ lib/frontend-helpers/html5_helper.rb
@@ -1,31 +1,18 @@
 module FrontendHelpers
   module Html5Helper
 
-    def ie_tag(name=:body, attrs={}, &block)
+    def html_tag(attrs = {}, &block)
       attrs.symbolize_keys!
-      haml_concat("<!--[if lt IE 7]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->".html_safe)
-      haml_concat("<!--[if IE 7]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->".html_safe)
-      haml_concat("<!--[if IE 8]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->".html_safe)
-      haml_concat("<!--[if gt IE 8]><!-->".html_safe)
-      haml_tag name, attrs do
-        haml_concat("<!--<![endif]-->".html_safe)
+      haml_concat("<!--[if lt IE 7]><html class='ie6 no-js'><![endif]-->")
+      haml_concat("<!--[if IE 7]><html class='ie7 no-js'><![endif]-->")
+      haml_concat("<!--[if IE 8]><html class='ie8 no-js'><![endif]-->")
+      haml_concat("<!--[if gt IE 8]><!-->")
+      haml_tag :html, attrs do
+        haml_concat("<!--<![endif]-->")
         block.call
       end
     end
 
-    def ie_html(attrs={}, &block)
-      ie_tag(:html, attrs, &block)
-    end
-
-  private
-
-    def add_class(name, attrs)
-      classes = attrs[:class] || ''
-      classes.strip!
-      classes = ' ' + classes if !classes.blank?
-      classes = name + classes
-      attrs.merge(:class => classes)
-    end
   end
 end
 
